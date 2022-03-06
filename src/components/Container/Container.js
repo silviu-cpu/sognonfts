a5bb72f8410399d44022d4d58b9a376c937f9673
Silviu Barbu <70367652+silviu-cpu@users.noreply.github.com>
2022-03-06 14:56:28 +0200

    Added Footer and Container

---
+++ src/components/Container/Container.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Container.module.css';
+
+const Container = () => (
+  <div className={styles.Container}>
+    Container Component
+  </div>
+);
+
+Container.propTypes = {};
+
+Container.defaultProps = {};
+
+export default Container;
---
+++ src/components/Container/Container.stories.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+import Container from './Container';
+
+export default {
+  title: "Container",
+};
+
+export const Default = () => <Container />;
+
+Default.story = {
+  name: 'default',
+};
