43a9ab308f446989b4ec7fe61c5b14a7b571b739
Silviu Barbu <70367652+silviu-cpu@users.noreply.github.com>
2022-03-06 15:02:53 +0200

    Added container

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
+import Container from './components/Container/Container';
 
 function App() {
   return (
     <div>
       <Navbar/>
       <Footer/>
+      <Container/>
     </div>
   );
 }
