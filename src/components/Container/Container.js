a333045039723c7a806d770275ef4c7633befad8
Silviu Barbu <70367652+silviu-cpu@users.noreply.github.com>
2022-03-08 22:43:46 +0200

    Added grid

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -2,8 +2,91 @@ import React from 'react';
 import styles from './Container.module.css';
 
 const Container = () => (
-  <div className="container justify-content-center d-flex" >
-    Container Component
+  <div className={styles.containers} >
+    <div className={`${styles.bg1} ${styles.plm}`}>
+        <h1>PRA</h1>
+        <p>Goals Completed</p>
+    </div>
+    <div className={styles.bg1}>
+        <h1>PRA asdasda xxx</h1>
+        <p>PORC TV</p>
+    </div>
+    <div className={styles.bg1}>
+        <h1>Cei care nu se conformeaza</h1>
+        <p>Jur de cativa ani parca lumea mi-e ciudata</p>
+    </div>
+    <div className={styles.bg1}>
+        <h1>Povestea daca-i spusa brici</h1>
+        <p>TImpu e plin de siretlicuri</p>
+    </div>
+    <div className={styles.bg1}>
+        <h1>Inca n-am cazut prada</h1>
+        <p>Mi e greu sa fiu chill, e clar dificil</p>
+    </div>
+    <div className={styles.bg1}>
+        <h1>PRA takfskaskfaksfs</h1>
+        <p>Goals Completedasdasdasdasda </p>
+    </div>
+    <div className={styles.bg1}>
+        <h1>De unde </h1>
+        <p>Goals Completed a23232 32 32 23 </p>
+    </div>
+    <div className={styles.bg1}>
+        <h1>Putere </h1>
+        <p>Goals Completed aaaa</p>
+    </div>
+    <div className={styles.bg2}>
+        <h1>PRA</h1>
+        <p>Goals Completed XXX</p>
+    </div>
+    <div className={styles.bg2}>
+        <h1>PRA</h1>
+        <p>Ca m-apuc de portocale</p>
+    </div>
+    <div className={styles.bg2}>
+        <h1>PRA</h1>
+        <p>Duma dupa duma dupa duma</p>
+    </div>
+    <div className={styles.bg2}>
+        <h1>PRA</h1>
+        <p>Duma dupa duma dupa duma</p>
+    </div>
+    <div className={styles.bg2}>
+        <h1>PRA</h1>
+        <p>Duma dupa duma dupa duma</p>
+    </div>
+    <div className={styles.bg2}>
+        <h1>PRA</h1>
+        <p>Duma dupa duma dupa duma</p>
+    </div>
+    <div className={styles.bg2}>
+        <h1>PRA</h1>
+        <p>Duma dupa duma dupa duma</p>
+    </div>
+    <div className={styles.bg2}>
+        
+    </div>
+    <div className={styles.bg2}>
+        
+    </div>
+    <div className={styles.bg2}>
+        
+    </div>
+    <div className={styles.bg2}>
+        
+    </div>
+    <div className={styles.bg2}>
+    
+    </div>
+    <div className={styles.bg2}>
+    
+    </div>
+    <div className={styles.bg2}>
+    
+    </div>
+    <div className={styles.bg2}>
+    
+    </div>
   </div>
 );
 
