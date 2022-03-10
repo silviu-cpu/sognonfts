9ef921cae409dd196a8d71c206e7b6b056fdfb46
Silviu Barbu <70367652+silviu-cpu@users.noreply.github.com>
2022-03-10 15:39:35 +0200

    Changed

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -2,21 +2,11 @@ import React from 'react';
 import styles from './Container.module.css';
 
 const Container = () => (
-  <div className="container">
   <div className={styles.containers}>
     <div className={`${styles.bg1} ${styles.part}`}>
         <h1>PRA</h1>
         <p>Goals Completed</p>
     </div>
-    <div className={styles.bg1}>
-        <h1>PRA asdasda xxx</h1>
-        <p>PORC TV</p>
-    </div>
-    <div className={styles.bg1}>
-        <h1>PRA asdasda xxx</h1>
-        <p>PORC TV</p>
-    </div>
-  </div>
   </div>
 );
 
